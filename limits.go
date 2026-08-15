4714a0c7b1d26040776aa62c8acd3b077d23da1f
devosher01 <devosher01@gmail.com>
2026-08-14 20:03:40 -0500

    feat: add minimal engine with wal-backed recovery

---
+++ limits.go
@@ -0,0 +1,20 @@
+package cairn
+
+const (
+	MaxKeySize   = 64 << 10
+	MaxValueSize = 4 << 20
+)
+
+func validateKey(key []byte) error {
+	if len(key) == 0 || len(key) > MaxKeySize {
+		return ErrInvalidKey
+	}
+	return nil
+}
+
+func validateValue(value []byte) error {
+	if len(value) > MaxValueSize {
+		return ErrInvalidValue
+	}
+	return nil
+}
